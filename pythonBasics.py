a2b47eb8f69cc1b53e3a8663b3488d3efba90195
ManateeMan <96268355+IceLeviathan444@users.noreply.github.com>
2023-11-03 12:28:39 -0400

    add list testing

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,5 +1,7 @@
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-sum = float(num1 + num2)
-print(sum)
+nameList = ["Jack", "Jimmy", "Joe", "Jim", "Jen"]
+print("There are" , len(nameList) , "names in nameList.")
+print("The 2nd name is", nameList[2])
+print("The last name is", nameList[-1])
+print("The names from name #1 to name #3 are:", nameList[0:3]) 
+print("The 4th name is", nameList[3])
+print("The names from the beginning to the third name are:", nameList[:3])
