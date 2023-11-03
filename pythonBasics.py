485209042361123af8fcbe8d17883e6f8fe4699c
ManateeMan <96268355+IceLeviathan444@users.noreply.github.com>
2023-11-03 11:58:14 -0400

    add input

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,4 +1,2 @@
-testList = ["abc", 34, True, 40.5, "Male"]
-print(testList)
-
-print("The length of testList is", len(testList))
+name = input("What's your name?")
+print("Hello my name is " + name " !")
