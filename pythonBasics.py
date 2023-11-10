88f965f4c11bd4b2779befdb1d3848778927dba5
ManateeMan <96268355+IceLeviathan444@users.noreply.github.com>
2023-11-10 12:03:40 -0500

    add input

--- pythonBasics.py
+++ pythonBasics.py
@@ -1,7 +1,10 @@
-nameList = ["Jack", "Jimmy", "Joe", "Jim", "Jen"]
+nameList = ["Placeholder", "Jimmy", "Joe", "Jim", "James"]
+nameList[0] = input("Please input the 1rst name\n")
 print("There are" , len(nameList) , "names in nameList.")
-print("The 2nd name is", nameList[2])
+print("The 3rd name is", nameList[2])
 print("The last name is", nameList[-1])
 print("The names from name #1 to name #3 are:", nameList[0:3]) 
 print("The 4th name is", nameList[3])
-print("The names from the beginning to the third name are:", nameList[:3])
+print("The names from the beginning to the third name are:", nameList[:5])
+
+print("Jimmy + James = " + (nameList[1] + nameList[4]))
